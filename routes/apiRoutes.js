03c0d7b16e8a4d42ff89dbfeea32dbcf3866a792
jasmo2 <jasmo2.com@gmail.com>
2017-03-23 19:11:33 -0500

    alert on user not found

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,35 +15,31 @@ const client = new Twitter({
   access_token_secret: accessTokenSecret,
 });
 
-router.get('/user_a', (req, res) => { //eslint-disable-line
-  console.log(consumerKey);
-  console.log(consumerSecret);
-  console.log(accessTokenKey);
-  console.log(accessTokenSecret);
-  client.get('followers/list', {
-      screen_name: 'alexpolymath',
-      stringify_ids: true,
-      count: 5,
-      cursor: '1561778087509337600'
-  }).then((response) => {
-      console.log(response);
-      res.json(response);
+router.get('/user/:user_name', (req, res) => { //eslint-disable-line
+  const userName = req.params.user_name;
+  console.log(`req.params.user_name: ${userName}`);
+  client.get('users/show.json', {
+    screen_name: userName,
+    profile_image_url: true
+  }).then(response => {
+    console.log(response);
+    res.json(response);
   }).catch(e => {
-    console.log('~~~~~~~error~~~~`');
+    console.log('~~~~~~~error~~~~~');
     console.error(e);
+    res.status(404).json({ error: 'user not found' });
   });
-});
-
-router.get('/user_b', (req, res) => { //eslint-disable-line
-  client.get('followers/list', {
-      screen_name: 'alexpolymath',
-      stringify_ids: true,
-      count: 5,
-      cursor: '1561778087509337600'
-    }).then((response) => {
-      console.log(response);
-      res.json(response);
-    });
+  // client.get('followers/list', {
+  //     screen_name: userName, //alexpolymath
+  //     stringify_ids: true,
+  //     count: 5,
+  // }).then(response => {
+  //     console.log(response);
+  //     res.json(response);
+  // }).catch(e => {
+  //   console.log('~~~~~~~error~~~~~');
+  //   console.error(e);
+  // });
 });
 
 export default router;
--- views/src/containers/userTwitter.js
+++ views/src/containers/userTwitter.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import SweetAlert from 'sweetalert-react';
 
 class UserTwitter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { show: false, error: null };
+	}
+
 	handleSubmit(event) {
 		event.preventDefault();
 		const userName = this.refs.inputTwitterUser.value; //eslint-disable-line
@@ -10,19 +16,28 @@ class UserTwitter extends Component {
 			console.log(response.data);
 		})
 		.catch((error) => {
-			console.log(error);
+			const err = error.response.data.error;
+			console.log(err);
+			this.setState({ show: true, error: err });
 		});
 	}
 
 	render() {
 		return (
 			<div className='container'>
+				<SweetAlert
+					show={this.state.show}
+					title="Error"
+					type="warning"
+					text={this.state.error}
+					onConfirm={() => this.setState({ show: false })}
+				/>
 				<form className='form-signin' onSubmit={this.handleSubmit.bind(this)} >
 					<h2 className='form-signin-heading'>{ this.props.title }</h2>
 					<label htmlFor='inputEmail' className='sr-only'>Twitter User</label>
 					<input
 						type='text' ref='inputTwitterUser' className='form-control'
-						placeholder='eg: jasmo2' required='' autoFocus=''
+						placeholder='eg: ComJasmo2' required='' autoFocus=''
 					/>
 				<button className='btn btn-lg btn-primary btn-block' type='submit'>Get Followers</button>
 				</form>
