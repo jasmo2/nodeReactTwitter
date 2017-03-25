88cebdfcd6a9b1e11e9d4c8990a538b909355d3a
jasmo2 <jasmo2.com@gmail.com>
2017-03-24 20:20:09 -0500

    userTwitter Conatiner add onChangeUser

--- views/src/containers/userTwitter.js
+++ views/src/containers/userTwitter.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import SweetAlert from 'sweetalert-react';
+import { connect } from 'react-redux';
+import { addUserA, addUserB } from '../actions';
 
 class UserTwitter extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { show: false, error: null };
+		this.onChangeUser = this.onChangeUser.bind(this);
 	}
 
-	handleSubmit(event) {
+	onChangeUser(event) {
 		event.preventDefault();
 		const userName = this.refs.inputTwitterUser.value; //eslint-disable-line
-		axios.get(`/api/user/${userName}`)
-		.then((response) => {
-			console.log(response.data);
-		})
-		.catch((error) => {
-			const err = error.response.data.error;
-			console.log(err);
-			this.setState({ show: true, error: err });
-		});
+
+		if (this.props.userType === 'A') {
+			this.props.addUserA(userName);
+		} else if (this.props.userType === 'B') {
+			this.props.addUserB(userName);
+		}
 	}
 
 	render() {
 		return (
 			<div className='container'>
-				<SweetAlert
-					show={this.state.show}
-					title="Error"
-					type="warning"
-					text={this.state.error}
-					onConfirm={() => this.setState({ show: false })}
-				/>
-				<form className='form-signin' onSubmit={this.handleSubmit.bind(this)} >
+				<form className='form-signin' >
 					<h2 className='form-signin-heading'>{ this.props.title }</h2>
 					<label htmlFor='inputEmail' className='sr-only'>Twitter User</label>
 					<input
-						type='text' ref='inputTwitterUser' className='form-control'
+						type='text' ref='inputTwitterUser' onChange={this.onChangeUser} className='form-control'
 						placeholder='eg: ComJasmo2' required='' autoFocus=''
 					/>
-				<button className='btn btn-lg btn-primary btn-block' type='submit'>Get Followers</button>
 				</form>
 			</div>
 		);
 	}
 }
 
-export default UserTwitter;
+
+const mapDispatchToProps = {
+  addUserA, addUserB
+};
+
+export default connect(null, mapDispatchToProps)(UserTwitter);
