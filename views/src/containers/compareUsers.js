e880404bc75d3d7324188556e583288761d91e64
jasmo2 <jasmo2.com@gmail.com>
2017-03-24 20:36:09 -0500

    add loading state

--- views/src/containers/compareUsers.js
+++ views/src/containers/compareUsers.js
@@ -7,21 +7,23 @@ import { addFollowers } from '../actions';
 class CompareButton extends Component {
     constructor(props) {
       super(props);
-      this.state = { show: false, error: null };
+      this.state = { show: false, error: null, loading: false };
       this.compare = this.compare.bind(this);
     }
     compare() {
       const { userA, userB } = this.props.users;
       if (userA && userB) {
+        this.setState({ loading: true });
         axios.get(`/api/common-followers/${userA}/${userB}`)
         .then((response) => {
           console.log(response.data);
+          this.setState({ loading: false });
           // this.props.addFollowers(response.data);
         })
         .catch((error) => {
           const err = error.response.data.error;
           console.log(err);
-          this.setState({ show: true, error: err });
+          this.setState({ show: true, error: err, loading: false });
         });
       } else {
         this.setState({ show: true, error: 'set the two users' });
@@ -37,9 +39,15 @@ class CompareButton extends Component {
               text={this.state.error}
               onConfirm={() => this.setState({ show: false })}
             />
-            <button onClick={this.compare} className='btn-block btn-success'>
+            { this.state.loading ?
+            <button className="btn-lg btn-success">
+              <span className="glyphicon glyphicon-refresh glyphicon-refresh-animate" />
+              Loading...
+            </button> :
+            <button onClick={this.compare} className='btn-lg btn-block btn-success'>
               {this.props.title}
             </button>
+            }
           </div>
         );
     }
