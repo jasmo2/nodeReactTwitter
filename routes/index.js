9064c040a86fb1c86d453fd7a5924c7aca97b237
jasmo2 <jasmo2.com@gmail.com>
2017-03-26 09:42:38 -0500

    Add 404 page

--- routes/index.js
+++ routes/index.js
@@ -52,12 +52,15 @@ router.get('/', (req, res) => {
 
 			res.status(200).send(renderFullPage(html, finalState));
 		} else {
-			res.status(404).send('Not found');
+			res.status(404).send(page404());
 		}
 	});
 });
 
 router.use('/api', apiRoutes);
+router.use('*', (req, res) => {
+  res.status(404).send(page404());
+});
 /*
 In this function, you can render you html part of the webpage. You can add some
 meta tags or Opern Graph tags using JS variables.
@@ -93,5 +96,47 @@ function renderFullPage(html, initialState) {
     </html>
     `;
 }
+
+function page404() {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    	<!-- Required meta tags always come first -->
+    	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    	<title>404 React Router Redux Express</title>
+
+    	<!-- Bootstrap CSS -->
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    	<link rel="stylesheet" href="../stylesheets/404.css">
+    </head>
+    <body>
+
+      <div class="error">
+          <div class="error-code m-b-10 m-t-20">404 <i class="fa fa-warning"></i></div>
+          <h3 class="font-bold">We couldn't find the page..</h3>
+
+          <div class="error-desc">
+            Sorry, but the page you are looking for was either not found or does not exist. <br/>
+          Try refreshing the page or click the button below to go back to the Homepage.
+          <div>
+            <a class=" login-detail-panel-button btn" href="/">
+              <i class="fa fa-arrow-left"></i>
+              Go back to Homepage
+            </a>
+          </div>
+        </div>
+      </div>
+    	<!-- jQuery first, then Bootstrap JS. -->
+    	<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `;
+}
+
 
 export default router;
